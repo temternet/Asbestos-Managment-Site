dc727effa2f830ab3ccab8012b5344a1cc1d3a81
Tim Clarkson <clarksontim@outlook.com>
2020-04-16 07:14:42 +0100

    Update index.php
    + modified index.php to get site to route to landing page rather than the default laravel page

--- public/index.php
+++ public/index.php
@@ -23,7 +23,7 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
 
-require __DIR__.'/../resources/views/welcome.blade.php';
+require __DIR__.'/../resources/views/landing.blade.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -37,7 +37,7 @@ require __DIR__.'/../resources/views/welcome.blade.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+//$app = require_once __DIR__.'/../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
