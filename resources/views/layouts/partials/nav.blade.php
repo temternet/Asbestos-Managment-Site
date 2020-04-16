477ac7ecdf91c22a3eea6728abe56257c966402c
tEMternette <tem@temter.net>
2020-04-16 06:58:57 +0100

    Bootstrap theme template test

---
+++ resources/views/layouts/partials/nav.blade.php
@@ -0,0 +1,18 @@
+<div class="collapse bg-inverse" id="navbarHeader">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 py-4">
+                <h4 class="text-white">Login</h4>
+                <p class="text-muted">Login</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="navbar navbar-inverse bg-inverse">
+    <div class="container d-flex justify-content-betIen">
+        <a href="#" class="navbar-brand">Asbestos Manager</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+</div>
