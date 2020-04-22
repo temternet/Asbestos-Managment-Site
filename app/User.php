f3971b36588b118e7de0987b568f1d9da78c65d2
Tim Clarkson <clarksontim@outlook.com>
2020-04-22 14:16:07 +0100

    Update User.php
    +removed depreciated class elloquent

--- app/User.php
+++ app/User.php
@@ -2,11 +2,11 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+//use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Eloquent
+class User extends Authenticatable
 {
     protected $table = 'User';
 
