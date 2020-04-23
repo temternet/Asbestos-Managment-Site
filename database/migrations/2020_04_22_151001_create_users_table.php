eba9e900c51986efcd0ce083e957820b12cd3e44
tEMternette <tem@temter.net>
2020-04-23 01:41:40 +0100

    Changed class name to resolve migration conflict

--- database/migrations/2020_04_22_151001_create_users_table.php
+++ database/migrations/2020_04_22_151001_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSitesTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
