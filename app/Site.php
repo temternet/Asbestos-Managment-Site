05d41d308183528118ba0c15645ab1aadc898159
tEMternette <tem@temter.net>
2020-04-23 01:31:15 +0100

    Updated Sites and Users models

--- app/Site.php
+++ app/Site.php
@@ -3,8 +3,33 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Site extends Model
 {
-    //
+    protected $table = 'Site';
+
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'siteID', 'addressL1', 'addressL2', 'town', 'county', 'postCode', 'lAuth',
+    ];
+
+   /* protected $guarded = [
+        'userID',  'certExpiry',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+
+    public $timestamps = false;
 }
