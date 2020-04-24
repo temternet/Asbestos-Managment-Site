c3071a6c0a396d54adc2e37c64beba23d1508ea4
Tim Clarkson <clarksontim@outlook.com>
2020-04-24 09:36:28 +0100

    minor tweeks to the footer layout

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -1,8 +1,10 @@
-<footer class="page-footer text-muted blue pt-4">
-    <div class="container">
+<footer class="page-footer font-small blue">
+    <div class="col-xs-11">
         <div class="footer-copyright text-left py-4">Timothy Clarkson 2020:
             <a href="https://github.com/tEMternette/Asbestos-Managment-Site.git"> github repo</a>
         </div>
+    </div>
+    <div class="col-xs-1">
         <p class="float-right">
             <a href="#">Back to top</a>
         </p>
