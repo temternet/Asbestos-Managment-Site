43f55424c9c348a86c235476600e56426811ce24
Tim Clarkson <clarksontim@outlook.com>
2020-04-26 10:39:29 +0100

    Update AsbestosRecordEntitySeeder.php

--- database/seeds/AsbestosRecordEntitySeeder.php
+++ database/seeds/AsbestosRecordEntitySeeder.php
@@ -14,6 +14,6 @@ class AsbestosRecordEntitySeeder extends Seeder
     {
         //
         $count = 10;
-        factory(AsbestosRecordSeeder::class, $count)->create();
+        factory(AsbestosRecordEntitySeeder::class, $count)->create();
     }
 }
