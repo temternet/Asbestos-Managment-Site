02bc5d61a62d6645cf3ccbe1c3cc86924532c127
tEMternette <tem@temter.net>
2020-04-26 16:44:15 +0100

    added premises controller

---
+++ database/seeds/AsbestosPlanSe.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AsbestosPlanSe extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
