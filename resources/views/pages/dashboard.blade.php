1c9ff9a600f6194a6f305f3dbfa18d1c1f12d0e0
Tim Clarkson <clarksontim@outlook.com>
2020-04-26 17:04:15 +0100

    fixing app breaking spelling mistakes

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -44,12 +44,12 @@
                     </tr>
                     @foreach($data as $row)
                       <tr>
-                        <td><a href="/premises/{{$row->premesisID}}">1</a></td>
-                        <td><a href="/premises/{{$row->premesisID}}">{{ $row->orgName }}</a></td>                    
-                        <td><a href="/premises/{{$row->premesisID}}">{{ $row->premisesAdr . ', ' . $row->addressL1 . ', ' . 
+                        <td><a href="/premises/{{$row->premisesID}}">1</a></td>
+                        <td><a href="/premises/{{$row->premisesID}}">{{ $row->orgName }}</a></td>                    
+                        <td><a href="/premises/{{$row->premisesID}}">{{ $row->premisesAdr . ', ' . $row->addressL1 . ', ' . 
                         $row->addressL2 . ', ' . $row->town . ', ' . $row->county . ', ' . $row->postCode }}</a></td>
-                        <td><a href="/premises/{{$row->premesisID}}">{{ $row->foreName . ' ' . $row->surName }}</td>
-                        <td><a href="/premises/{{$row->premesisID}}">{{ $row->monitorDate }}</a></td>
+                        <td><a href="/premises/{{$row->premisesID}}">{{ $row->foreName . ' ' . $row->surName }}</td>
+                        <td><a href="/premises/{{$row->premisesID}}">{{ $row->monitorDate }}</a></td>
                       </tr>
                     @endforeach
                   </thread>
